88b5767e6777ebfa7386a3b1785ba35ae6a22383
madtitan1712 <vasusudhan123@gmail.com>
2024-08-23 16:32:35 +0530

    Added code

--- 23August/main.py
+++ 23August/main.py
@@ -10,3 +10,26 @@ youngest=df.age.min()
 oldest=df.age.max()
 print(f"length is {length}\n youngest is: {youngest}\n oldest is :{oldest}")
 df=df.rename(columns={'educ':'education'})
+def get_party(df):
+  if df['PID'] < 2:
+    return "Democrat"
+  elif df['PID'] > 4:
+    return "Republican"
+  else:
+    return "Independent"
+df['party'] = df.apply(get_party, axis = 1)
+def get_agegroup(df):
+  if df['age'] < 25:
+    return "18-24"
+  elif df['age'] < 35:
+    return "25-34"
+  elif df['age'] < 45:
+    return "35-44"
+  elif df['age'] < 55:
+    return "45-54"
+  elif df['age'] < 65:
+    return "55-64"
+  else:
+    return "65 and over"
+df['age_group'] = df.apply(get_agegroup, axis = 1)
+print(df)
